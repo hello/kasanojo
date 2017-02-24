7b8edcece719ff85e86f7e5bcbba70ac676345fd
Jackson Chen <jchen@sayhello.com>
2017-02-24 15:30:44 -0800

    ssh agent forwarding

--- tools/test_run.sh
+++ tools/test_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker build -t kasanojo . && sudo docker run -t --rm -i kasanojo bash
+sudo docker build -t kasanojo . && sudo docker run -t --rm -i -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
