89688cf62949c1a86d5124072f50ddce05b272de
Jackson Chen <jchen@sayhello.com>
2017-02-24 16:42:31 -0800

    mount git
    change directory if needed

--- tools/test_run.sh
+++ tools/test_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #sudo docker build -t kasanojo . && sudo docker run -t --rm -i -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
-sudo docker build -t kasanojo . && sudo docker run -t -i -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
+sudo docker build -t kasanojo . && sudo docker run -t -i -v  /home/jchen/Projects/kasa:/home/hello/kasa -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
