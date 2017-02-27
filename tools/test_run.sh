06b1d871d7cc6a8de1be440f7cc9ea145f40de79
Jackson Chen <jchen@sayhello.com>
2017-02-27 14:01:08 -0800

    mount scripts folder

--- tools/test_run.sh
+++ tools/test_run.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 #sudo docker build -t kasanojo . && sudo docker run -t --rm -i -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
-sudo docker build -t kasanojo . && sudo docker run -t -i -v  /home/jchen/Projects/kasa:/home/hello/kasa -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+echo $DIR
+
+sudo docker build -t kasanojo . && sudo docker run -t -i \
+    -v  /home/jchen/Projects/kasa:/home/hello/kasa \
+    -v $DIR/../scripts:/home/hello/scripts \
+    -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
