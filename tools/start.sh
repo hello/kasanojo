9854887bd1e6703e85f0cdbb85eaff12dfb7fd09
Jackson Chen <jchen@sayhello.com>
2017-02-27 15:41:57 -0800

    Revert "chain args"
    This reverts commit f0b2f81898b44b7ba4b87a996106e96d1ee787df.

--- tools/start.sh
+++ tools/start.sh
@@ -3,25 +3,17 @@ set -e
 #sudo docker build -t kasanojo . && sudo docker run -t --rm -i -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 KASADIR=$DIR/../../kasa
-EXT_ARGS="cd /home/hello"
 
-if [ $# -eq 0 ]
-then
-    echo "No arguments supplied"
-else
-    EXT_ARGS+=" && ./$1"
-
-fi
 echo ===========================
-echo Tools:   $DIR
+echo Docker:  $DIR
 echo Project: $KASADIR
-echo Args:    $EXT_ARGS
 echo ===========================
+
 if [ -d $KASADIR ]; then
     sudo docker build -t kasanojo . && sudo docker run --rm -t -i \
         -v $KASADIR:/home/hello/kasa \
         -v $DIR/../scripts:/home/hello/scripts:ro \
-        -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash -c "$EXT_ARGS"
+        -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent kasanojo /bin/bash
     exit 0
 else
     echo Unable to find build directory, please checkout repo to $KASADIR
