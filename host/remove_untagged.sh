63fc13364402e76207814141416d10d03007ca02
Jackson Chen <jchen@sayhello.com>
2017-03-16 15:30:45 -0700

    no space

--- host/remove_untagged.sh
+++ host/remove_untagged.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Delete all stopped containers
-sudo docker rm $( sudo docker ps -q -f status=exited)
+sudo docker rm $(sudo docker ps -q -f status=exited)
 # Delete all dangling (unused) images
-sudo docker rmi $( sudo docker images -q -f dangling=true)
+sudo docker rmi $(sudo docker images -q -f dangling=true)
 sudo docker images
