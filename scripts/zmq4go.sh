67f29ec0566e596f442b14bed0c65f20b4d93f38
plasticchris <plasticchris@gmail.com>
2017-03-23 17:52:04 -0700

    convenience go cross scripts

--- scripts/zmq4go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SYSROOT=/home/hello/kasa/ambarella/out/hawthorn/fakeroot
-
-TOOLCHAIN=/usr/local/linaro-armv7ahf-2015.11-gcc5.2/bin/arm-linux-gnueabihf-gcc
-
-env CGO_ENABLED=1 C_INCLUDE_PATH=$SYSROOT/usr/include/ LIBRARY_PATH=$SYSROOT/usr/lib CC=$TOOLCHAIN PKG_CONFIG_PATH=$SYSROOT/usr/lib/pkgconfig/ GOOS=linux GOARCH=arm GOARM=7 go get github.com/pebbe/zmq4
