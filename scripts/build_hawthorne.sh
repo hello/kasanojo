06c28a11ae336ab7cd827b05e19324292151c3d2
plasticchris <plasticchris@gmail.com>
2017-03-23 17:33:54 -0700

    don't source env again

--- scripts/build_hawthorne.sh
+++ scripts/build_hawthorne.sh
@@ -3,7 +3,6 @@ set -e
 
 echo Building Hawthorn 
 cd /home/hello/kasa/ambarella/boards/hawthorn
-source ../../build/env/armv7ahf-linaro-gcc.env
 make sync_build_mkcfg
 make s2l_ipcam_config 
 make defconfig_public_linux
